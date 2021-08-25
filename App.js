ce7600cf66e54126c74f61e20a2ae413d81a0f77
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2021-08-25 10:02:31 -0300

    Importando componente

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import OlaMundo from './src/components/OlaMundo';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <View>
-      <Text>Olá Mundo!</Text>
+      <OlaMundo/>
+      <Text style={styles.sectionTitle}>Olá Mundo! {count}</Text>
+      <Button title="Clique aqui" onPress={() => setCount(count + 1)}/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
---
+++ src/components/OlaMundo.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+const OlaMundo = () => {
+  return (
+    <View>
+      <Text>Olá Mundo!</Text>
+    </View>
+  );
+};
+
+export default OlaMundo;
